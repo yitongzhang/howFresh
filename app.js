9aeff234e84d57131b5a8bb93941016a839d0306
Yitong <zha.yitong@gmail.com>
2017-05-19 14:41:19 -0700

    Colors now change per season

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // Data =======================================
-var veggieDict = data
+var veggieDict = data;
 var monthsConvert ={"January":0, "February":1, "March":2, "April":3, "May":4, "June":5, "July":6, "August":7, "September":8, "October":9, "November":10, "December":11}
 var reMonthsConvert ={0:"January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June", 6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"}
 var months =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var seasonStart ={"Spring":"2015-03-21","Summer":"2015-06-21","Fall":"2015-09-21","Winter":"2015-12-21"}
+var seasons ={
+  "Spring":[2,3,4],
+  "Summer":[5,6,7],
+  "Fall":[8,9,10],
+  "Winter":[11,0,1]
+}
+var seasonColors={
+  "Spring":['#027003','#EEFEEF','#F8FFF6'],
+  "Summer":['#F1B005','#FEFDDE','#FFFCF0'],
+  "Fall":['#C84F30','#FDEBDD','#FFF8F2'],
+  "Winter":['#2269A1','#F1F9FF','#FAFDFF']
+}
 var CurrentMonth = new Date().getMonth()
 var CurrentDate = new Date().getDate()
 var CurrentYear = new Date(). getFullYear()
-// In number format
 var selectedMonths=[]
 selectedMonths.push(CurrentMonth)
-
+var CurrentSeason;
 
 // Components =================================
 
@@ -39,12 +49,19 @@ class MonthCheckList extends React.Component{
     var selected = this.num;
     selectedMonths=[];
     selectedMonths.push(selected);
+    for (var key in seasons) {
+      if (seasons[key].includes(selectedMonths[0])) {
+        CurrentSeason = key;
+      }
+    }
+    console.log("====selected month is "+selectedMonths)
+    console.log("====selected season is "+CurrentSeason)
+   
     this.setState(function(prevState, props) {
       return {
         isToggleOn: prevState.isToggleOn.map(
           function(currentValue, index, arr){
             if (selected==index) {
-              console.log("swapped "+selected)
               return true;
             }
             return false;
@@ -53,10 +70,12 @@ class MonthCheckList extends React.Component{
       };
     });
     ReactDOM.render( <App/> , document.getElementById('root'));
+    setTimeout(function(){setSeasonColor(CurrentSeason,seasonColors); }, 300);
   }
 
+
   render(){
-    console.log("====selected month is "+selectedMonths)
+    
     const months = this.props.months;
     const checkItems = months.map((month, i) =>
         <li key={month.toString()}>
@@ -81,8 +100,6 @@ class MonthCheckList extends React.Component{
         console.log('currentIndex', currentIndex);
         console.log('nextIndex', (currentIndex+1)%12);
         console.log('prevIndex', (currentIndex+11)%12);
-        console.log(this);
-        slickNext();
       }
     };
     if (winWidth>1200) {
@@ -221,8 +238,7 @@ function Body(){
 function Footer(){
   return(
     <footer className="twelve columns">
-      <div className="smallTitle"><div className="line left"></div><h5>About</h5><div className="line right"></div></div>
-      <p>Made by <a href="http://omstudio.co/">O/M Studio</a>, with data from <a href="http://www.cuesa.org/">CUESA</a></p>
+
     </footer>
   )
 }
@@ -261,38 +277,6 @@ function alphaToNumber(alphaList,monthsConvert){
   return numberList
 }
 
-// Event Handlers =============================
-function logMonthSelection(month,monthArray){
-  if (monthArray.includes(monthsConvert[month])) {
-    var index = monthArray.indexOf(monthsConvert[month])
-    
-    if (index > -1) {
-        monthArray.splice(index, 1);
-    }
-  }
-  else{
-     monthArray.push(monthsConvert[month])
-   }
-
-  ReactDOM.render( <App/> , document.getElementById('root'));
-}
-
-
-// Main ==========================
-ReactDOM.render( <App/> , document.getElementById('root'));
-window.onresize = function(event) {
-  setTimeout(function(){ ReactDOM.render( <App/> , document.getElementById('root')); }, 300);
-};
-
-window.onscroll = function() {
-  if (window.innerWidth < 720) {
-    stickMobileNav();
-  }
-  else {
-    stickDesktopNav();
-  }
-};
-
 function stickMobileNav (){
   var mobileSticky = document.getElementById('mobileMonthFilter');
   var stickyMain = document.getElementById('main');
@@ -319,5 +303,48 @@ function stickDesktopNav (){
   }
 }
 
+function setSeasonColor(CurrentSeason,seasonColors){
+  var monthButton = document.getElementsByTagName("button");
+  var vegItem = document.getElementsByClassName("vegItem");
+  var html = document.getElementsByTagName("html")[0];
+  var line = document.getElementsByClassName("line");
 
+  var textColor = seasonColors[CurrentSeason][0];
+  var itemColor = seasonColors[CurrentSeason][1];
+  var bgColor = seasonColors[CurrentSeason][2];
 
+  console.log(textColor);
+  console.log(itemColor);
+  console.log(bgColor);
+
+  html.style["background-color"]=bgColor;
+  html.style["color"]=textColor;
+
+  for (let button of monthButton) {
+    button.style["color"]=textColor;
+    button.style["background-color"]=bgColor;
+  }
+  for (let veg of vegItem) {
+    veg.style["background-color"]=itemColor;
+  }
+  for (let item of line) {
+    item.style["background-color"]=textColor;
+  }
+
+}
+
+// Main ==========================
+ReactDOM.render( <App/> , document.getElementById('root'));
+window.onresize = function(event) {
+  setTimeout(function(){ ReactDOM.render( <App/> , document.getElementById('root')); }, 300);
+};
+
+// Fixed navbar ==========================
+window.onscroll = function() {
+  if (window.innerWidth < 720) {
+    stickMobileNav();
+  }
+  else {
+    stickDesktopNav();
+  }
+};
